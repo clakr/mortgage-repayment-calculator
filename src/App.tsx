963fb6b9a014250aed39cbaa69960b0d182755c2
clakr <clarktolosa@gmail.com>
2024-07-04 11:10:20 +0800

    added design system

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
+import Form from "./Form";
+
 export default function App() {
   return (
     <>
       <h1 className="sr-only">Mortgage Repayment Calculator</h1>
-
+      <Form />
     </>
   )
 }
---
+++ src/Form.tsx
@@ -0,0 +1,6 @@
+export default function Form() {
+    return <form>
+        <section>left</section>
+        <section>right</section>
+    </form>
+}
