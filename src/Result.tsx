709663f87f67229c461b2aa6a453f3da97beeb0d
clakr <clarktolosa@gmail.com>
2024-07-04 13:58:40 +0800

    `Result` component layout

--- src/Result.tsx
+++ src/Result.tsx
@@ -1,3 +1,12 @@
 export default function Result() {
-  return <section className="bg-slate-900">result</section>;
+  return (
+    <section className="flex flex-col items-center justify-center gap-y-16 bg-slate-900 px-24 py-32 text-center tablet:p-40 desktop:rounded-es-[8rem]">
+      <img src="./illustration-empty.svg" alt="" />
+      <h3 className="font-preset-2 text-white">Results shown here</h3>
+      <p className="font-preset-4 text-slate-300">
+        Complete the form and click “calculate repayments” to see what your
+        monthly repayments would be.
+      </p>
+    </section>
+  );
 }
