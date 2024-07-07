72102051f4f5f3d22b86975d978f3ad416ac3e2e
clakr <clarktolosa@gmail.com>
2024-07-07 10:41:22 +0800

    pass down formdata using context

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,29 @@
-import { FormEvent } from "react";
+import { createContext, FormEvent, useState } from "react";
 import Calculator from "./Calculator";
 import Form from "./Form";
 import Result from "./Result";
 
+export const FormContext = createContext<FormData | null>(null);
+
 export default function App() {
+  const [form, setForm] = useState<FormData | null>(null);
+
   function handleSubmit(event: FormEvent<HTMLFormElement>) {
     event.preventDefault();
 
     const form = event.target as HTMLFormElement;
     const formData = new FormData(form);
 
-    console.log(Object.fromEntries(formData));
+    setForm(formData);
   }
 
   return (
-    <>
+    <FormContext.Provider value={form}>
       <h1 className="sr-only">Mortgage Repayment Calculator</h1>
       <Form onSubmit={handleSubmit}>
         <Calculator />
         <Result />
       </Form>
-    </>
+    </FormContext.Provider>
   );
 }
